105edfcbee9bb38daf2d0bd601d4413d352a85de
yumi kanebayashi <bon.kyumi@gmail.com>
2021-01-06 13:04:55 +0900

    Initial Commit

---
+++ DelegateButtonAlert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateButtonAlert
+//
+//  Created by yumi kanebayashi on 2021/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
